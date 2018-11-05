aef0a9382fd4fcac25f4d1b95ac50d27c68627a9
Cody Swartz <ctsstc@gmail.com>
2018-11-05 14:30:04 -0800

    More scaffolding

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  testURL: 'http://localhost',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "__tests__\/.*\.(test|spec)\.ts$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ]
+};
