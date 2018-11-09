d79fb48eae9c5feb3a821a72fe41f50dda8bab40
Cody Swartz <ctsstc@gmail.com>
2018-11-08 16:51:00 -0800

    Comment out global template until it's used somewhere.

--- __tests__/templates/index.ts
+++ __tests__/templates/index.ts
@@ -1,5 +1,6 @@
-import WindowLoader from '../helpers/windowLoader';
-import form from './partials/form';
-import iframe from './partials/iframe';
+// Commented out until this is in-use otherwise there's no tests to be ran for this code.
+// import WindowLoader from '../helpers/windowLoader';
+// import form from './partials/form';
+// import iframe from './partials/iframe';
 
-WindowLoader.loadElements(form, iframe);
+// WindowLoader.loadElements(form, iframe);
