1fe7f7122aeb8aad4e4a1759fed19338afd45af7
Cody Swartz <ctsstc@gmail.com>
2018-11-08 16:28:15 -0800

    Add typing to WindowLoader methods

--- __tests__/helpers/windowLoader.ts
+++ __tests__/helpers/windowLoader.ts
@@ -1,21 +1,21 @@
 export default class WindowLoader {
-  public static loadElements(...elements) {
+  public static loadElements(...elements: IPartial[]) {
     for (const elem of elements) {
       this.addElement(elem);
     }
   }
-  public static loadElement(element) {
+  public static loadElement(element: IPartial) {
     const newElement = this._initElement(element);
     document.body.innerHTML = newElement.outerHTML;
   }
-  public static addElement(element) {
+  public static addElement(element: IPartial) {
     const newElement = this._initElement(element);
     document.body.innerHTML += newElement.outerHTML;
   }
   public static clearElements() {
     document.body.innerHTML = '';
   }
-  private static _initElement(element) {
+  private static _initElement(element: IPartial) {
     const newElement = document.createElement(element.type);
     newElement.innerHTML = element.innerHtml;
     return newElement;
